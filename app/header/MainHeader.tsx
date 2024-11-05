1bc83b397f9201375e2a09a4ca546492f03e3506
Joaquin B <joaquin.bachler@gmail.com>
2024-11-05 18:13:26 -0300

    Ruta dinamica, funcion redirigir, header al main

--- app/header/MainHeader.tsx
+++ app/header/MainHeader.tsx
@@ -4,10 +4,12 @@ export default function Home() {
 
       <div className="flex items-center space-x-2">
         <img src="/assets/logo.jpg" alt="Logo" className="h-6 w-6" />
+        <a href="/"> 
         <h1 className="text-white font-bold">Ramos UC</h1>
+        </a>
       </div>
 
-      <button className="bg-amarillo text-white text-sm px-2 py-1 rounded">
+      <button className="bg-amarillo text-white text-sm px-2 py-1 rounded hover:bg-orange-200">
         Login
       </button>
     </header>
