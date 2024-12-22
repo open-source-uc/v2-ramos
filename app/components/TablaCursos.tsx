4692d3316a42ca899e7e92970776783b73488697
MangoFrapuccino <vcuis@estudiante.uc.cl>
2024-12-22 16:13:40 -0300

    eliminados botones, centrada la search bar y scroll a toda la view

--- app/components/TablaCursos.tsx
+++ app/components/TablaCursos.tsx
@@ -21,43 +21,39 @@ export default function TablaCursos({ cursos, isLogged }: TablaCursosProps) {
         );
       }, [searchTerm]);
 
-    return (
-        <div> 
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        <div className="mb-6">
-          <SearchBar value={searchTerm} onChange={setSearchTerm} />
-        </div>
-    </div>
-        <div className="relative overflow-x-auto mx-5 rounded-lg">
-        <div className="overflow-y-auto max-h-96">
+      return (
+        <div>
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+            <div className="mb-6 flex justify-center">
+              <SearchBar value={searchTerm} onChange={setSearchTerm} />
+            </div>
+          </div>
+    
+          <div className="mx-5 rounded-lg">
             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 sticky top-0 z-10">
-                    <tr>
-                        <th scope="col" className="px-6 py-3 text-center">NRC</th>
-                        <th scope="col" className="px-6 py-3 text-center">Nombre Curso</th>
-                        <th scope="col" className="px-6 py-3 text-center">Créditos</th>
-                        <th scope="col" className="px-6 py-3 text-center">+</th>
-                        <th scope="col" className="px-6 py-3 text-center">-</th>
-                        <th scope="col" className="px-6 py-3 text-center">Promedio</th>
-                        {isLogged && (
-                            <th scope="col" className="px-6 py-3 text-center">Vota!</th>
-                        )}
-                    </tr>
-                </thead>
-                <tbody>
-                    {filteredCourses.length > 0
-                        ? filteredCourses.map((curso, index) => (
-                              <CursoRow
-                                  key={index}
-                                  curso={curso}
-                                  isLogged={isLogged}
-                              />
-                          ))
-                        : null}
-                </tbody>
+              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 sticky top-0 z-10">
+                <tr>
+                  <th scope="col" className="px-6 py-3 text-center">NRC</th>
+                  <th scope="col" className="px-6 py-3 text-center">Nombre Curso</th>
+                  <th scope="col" className="px-6 py-3 text-center">Créditos</th>
+                  <th scope="col" className="px-6 py-3 text-center">+</th>
+                  <th scope="col" className="px-6 py-3 text-center">-</th>
+                  <th scope="col" className="px-6 py-3 text-center">Promedio</th>
+                </tr>
+              </thead>
+              <tbody>
+                {filteredCourses.length > 0
+                  ? filteredCourses.map((curso, index) => (
+                      <CursoRow
+                        key={index}
+                        curso={curso}
+                        isLogged={isLogged}
+                      />
+                    ))
+                  : null}
+              </tbody>
             </table>
+          </div>
         </div>
-    </div>
-    </div>
-    );
+      );
     }
