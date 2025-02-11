cbc2fa386a1ad0b829ec96e33c6f5366aee72726
Utmite <rockimunoz94@gmail.com>
2025-02-11 03:06:17 -0300

    update(node)

--- app/page.tsx
+++ app/page.tsx
@@ -65,7 +65,7 @@ export default async function page({ searchParams }: { searchParams: SearchParam
                 href={`/${course.sigle}`}
                 className="inline-flex items-center rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
-                Ver Críticasss
+                Ver Críticas
               </a>
             </section>
           </article>
