7f3ff05f2ac2b30e7315e4a19b6b7fafce0be09d
Utmite <rockimunoz94@gmail.com>
2025-05-26 21:46:39 -0400

    muerte

--- app/[sigle]/review/criticar.tsx
+++ app/[sigle]/review/criticar.tsx
@@ -1,25 +1,13 @@
-"use client";
+// "use client";
 
-import { useActionState } from "react";
+// import { useActionState } from "react";
 
-import { ReviewCreate } from "@/app/actions/reviews";
-import { Course } from "@/app/components/CourseInfo";
-import FormReview from "@/app/components/forms/FormReview";
+// // import { ReviewCreate } from "@/app/actions/reviews";
+// import { Course } from "@/app/components/CourseInfo";
+// import FormReview from "@/app/components/forms/FormReview";
 
-export default function Criticar({ course }: { course: Course }) {
-  const [state, action] = useActionState(ReviewCreate, {
-    errors: 0,
-    success: 0,
-    message: "",
-    body: {
-      course_id: course.course_id,
-      year: 2013,
-      section_number: 1,
-      liked: false,
-      comment: "",
-      estimated_credits: course.credits,
-    },
-  });
+// export default function Criticar({ course }: { course: Course }) {
 
-  return <FormReview state={state} action={action} />;
-}
+
+//   return <FormReview state={state} action={action} />;
+// }
