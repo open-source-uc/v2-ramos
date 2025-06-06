2c827b7f8db1c47deea1501675187fd55d33e172
Utmite <rockimunoz94@gmail.com>
2025-06-06 18:37:28 -0400

    k

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,19 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+
   adapter: cloudflare({
     platformProxy: {
       enabled: true
     }
-  })
+  }),
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
