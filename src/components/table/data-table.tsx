bb92dbe7c40e1929af92e8cfc59f455d4707430d
IgnacioPalma <ignaciopalma.w@gmail.com>
2025-06-19 16:45:53 -0400

    Improved filter

--- src/components/table/data-table.tsx
+++ src/components/table/data-table.tsx
@@ -9,7 +9,6 @@ import {
   useReactTable,
   getPaginationRowModel,
   getFilteredRowModel,
-  type ColumnFiltersState,
   getSortedRowModel,
   type SortingState,
 } from "@tanstack/react-table"
@@ -34,7 +33,7 @@ export function DataTable({
   data,
 }: DataTableProps) {
   const [sorting, setSorting] = useState<SortingState>([])
-  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
+  const [globalFilter, setGlobalFilter] = useState("")
   
   const table = useReactTable({
     data,
@@ -43,11 +42,19 @@ export function DataTable({
     getPaginationRowModel: getPaginationRowModel(),
     onSortingChange: setSorting,
     getSortedRowModel: getSortedRowModel(),
-    onColumnFiltersChange: setColumnFilters,
     getFilteredRowModel: getFilteredRowModel(),
+    globalFilterFn: (row, columnId, value) => {
+      const name = row.getValue("name") as string
+      const sigle = row.getValue("sigle") as string
+      const searchValue = value.toLowerCase()
+      
+      return name.toLowerCase().includes(searchValue) || 
+             sigle.toLowerCase().includes(searchValue)
+    },
+    onGlobalFilterChange: setGlobalFilter,
     state: {
       sorting,
-      columnFilters,
+      globalFilter,
     }
   })
 
@@ -55,38 +62,14 @@ export function DataTable({
     <div>
       <div className="flex items-center py-4 gap-4">
         <Input
-          placeholder="Buscar por nombre..."
-          value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+          placeholder="Buscar por nombre o sigla..."
+          value={globalFilter ?? ""}
           onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-            table.getColumn("name")?.setFilterValue(event.target.value)
-          }
-          className="max-w-sm"
-        />
-        <Input
-          placeholder="Buscar por sigla..."
-          value={(table.getColumn("sigle")?.getFilterValue() as string) ?? ""}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-            table.getColumn("sigle")?.setFilterValue(event.target.value)
+            setGlobalFilter(event.target.value)
           }
           className="max-w-sm"
         />
         <div className="space-x-2">
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
-            Anterior
-          </Button>
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-          >
-            Siguiente
-          </Button>
         </div>
       </div>
       <div className="rounded-md border border-foreground-muted-dark">
