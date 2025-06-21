3e3e71154a9baeb7b52e39bda4aece4ac7b57aca
Utmite <rockimunoz94@gmail.com>
2025-06-21 01:03:52 -0400

    l

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import cloudflare from '@astrojs/cloudflare';
 
 import tailwindcss from '@tailwindcss/vite';
 
-import svelte from '@astrojs/svelte';
-
 import react from '@astrojs/react';
 
 // https://astro.build/config
@@ -21,5 +19,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [svelte(), react()]
+  integrations: [react()]
 });
