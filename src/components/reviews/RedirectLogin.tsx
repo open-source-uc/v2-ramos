8a0a5bdf5fd58734c611b00542ce7e8203e91d0e
Utmite <rockimunoz94@gmail.com>
2025-06-21 15:23:58 -0400

    feat(RedirectLogin)

---
+++ src/components/reviews/RedirectLogin.tsx
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+
+export default function RedirectLogin({ userIsNull }: { userIsNull: boolean }) {
+    useEffect(() => {
+        if (userIsNull) {
+            window.location.href = "https://auth.osuc.dev";
+        }
+    }, [userIsNull]);
+
+    return null;
+}
