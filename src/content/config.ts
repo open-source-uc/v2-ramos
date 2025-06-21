55a8bdd4ac28be8eafda0206448503b758128e4b
Utmite <rockimunoz94@gmail.com>
2025-06-21 00:41:50 -0400

    k

--- src/content/config.ts
+++ src/content/config.ts
@@ -13,4 +13,47 @@ const recommendations = defineCollection({
     })
 })
 
-export const collections = { recommendations }
+const coursesScore = defineCollection({
+    loader: async () => {
+        const response = await fetch("http://localhost:4321/api/courses", {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${import.meta.env.API_SECRET}`,
+            }
+        });
+        const data = await response.json() as CourseSummary[];
+        console.log("Courses loaded:", data.length);
+        return data.map((c) => ({
+            id: c.id + "", // debe ser texto why, idk xd
+            sigle: c.sigle,
+            school_id: c.school_id,
+            area_id: c.area_id,
+            category_id: c.category_id,
+            superlikes: c.superlikes,
+            likes: c.likes,
+            dislikes: c.dislikes,
+            votes_low_workload: c.votes_low_workload,
+            votes_medium_workload: c.votes_medium_workload,
+            votes_high_workload: c.votes_high_workload,
+            avg_weekly_hours: c.avg_weekly_hours,
+            sort_index: c.sort_index
+        }))
+    },
+    schema: z.object({
+        id: z.string(),
+        sigle: z.string(),
+        school_id: z.number(),
+        area_id: z.number(),
+        category_id: z.number(),
+        superlikes: z.number(),
+        likes: z.number(),
+        dislikes: z.number(),
+        votes_low_workload: z.number(),
+        votes_medium_workload: z.number(),
+        votes_high_workload: z.number(),
+        avg_weekly_hours: z.number(),
+        sort_index: z.number()
+    })
+})
+
+export const collections = { recommendations, coursesScore };
