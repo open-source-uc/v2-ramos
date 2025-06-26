d6273b6446d8fe911d31ece8e03296e78fb4e265
Utmite <rockimunoz94@gmail.com>
2025-06-26 01:04:40 -0400

    l

--- migration/reload-courses.sh
+++ migration/reload-courses.sh
@@ -1,5 +1,5 @@
 curl -s -X GET "https://buscaramos.osuc.dev/api/courses" \
-  -H "Authorization: Bearer SZx823XH7SjxLlniVAxC" \
+  -H "Authorization: Bearer $API_SECRET" \
   -H "Accept: application/x-ndjson; charset=utf-8" \
 | npx wrangler r2 object put ousc-public/courses-score.ndjson --remote --pipe \
     --content-type "application/x-ndjson; charset=utf-8"
