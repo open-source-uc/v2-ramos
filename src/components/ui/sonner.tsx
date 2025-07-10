506a2d526405d73cb291685d523c7f4868e9423b
IgnacioPalma <ignaciopalma.w@gmail.com>
2025-07-10 14:22:19 -0400

    better sonner

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -5,6 +5,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
   return (
     <Sonner
       className="toaster group"
+      position="top-center"
       toastOptions={{
         classNames: {
           toast: "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
